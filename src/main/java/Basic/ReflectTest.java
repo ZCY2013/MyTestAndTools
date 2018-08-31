8631f1d50178c82d6a995bfa94cd9dda7abeaa8e
ZCY2013 <zcy32897629@126.com>
2018-08-31 22:49:54 +0800

    complete toString method

--- src/main/java/Basic/ReflectTest.java
+++ src/main/java/Basic/ReflectTest.java
@@ -3,12 +3,14 @@ package Basic;
 import JMX.StanderdMBean.Hello;
 
 public class ReflectTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         //array class  get special name
         System.out.println(Double[].class.getName()); // [Ljava.lang.Double;
 
         System.out.println(Hello[].class.getName());//[LJMX.StanderdMBean.Hello;
 
         System.out.println(int[].class.getName());//[I
+
+        Class.forName("java.lang.String");
     }
 }
